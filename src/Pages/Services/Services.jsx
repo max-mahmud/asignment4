913c69bfe03c244dd2058bfc6d3663e07f384323
max-mahmud <newmahmud24@gmail.com>
2023-03-09 10:57:15 -0800

    sing res done

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -11,7 +11,7 @@ const Services = () => {
   return (
     <>
     <Helmet title={'Service'}/>
-    <All_bg text={'our Service'} text2='home/service'/>
+    <All_bg text={'Service'} text2='home/service'/>
     <All_Info all_image={serviceImage} all_heading='Your business
     is our concern' all_sub_heading='Services' all_text="Although subscribers are welcome to come to our downtown office and pick up newspapers that did not get delivered, we know that isn’t an easy option for many of you. Some of you are retired and/or live outside of town."/>
     <Service_offer/>
